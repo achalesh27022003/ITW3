d606ebe829a064a71ac12bec8ef376aa94c39f12
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2020-10-27 14:37:20 +0530

    fixed save button

--- python_assignment_1.py
+++ python_assignment_1.py
@@ -7,9 +7,7 @@ from tkinter import *
 root=Tk()
 root.title("Plot Sin and Cos")
 
-
-root.configure(bg='red')
-root.geometry('700x500')
+root.geometry('500x300')
 
 
 
@@ -49,11 +47,9 @@ def show():
       time=2*np.pi
 
       amplitude =np.array([time,f1,f2,A1,A2])
-      c=np.savetxt('mycsv.csv',amplitude)#saving data into csv file
-
-      def readData():#fetching data from csv file
- 
-          q=np.loadtxt('mycsv.csv');
+      np.savetxt('mycsv.csv',amplitude)
+      def readData():
+          q=np.loadtxt('mycsv.csv')
           return q
 
       a=readData() 
@@ -89,12 +85,50 @@ def show():
       plot.show()
 
 def save():
+      f1=int(txt1.get())
+      A1=int(txt2.get())
+      f2=int(txt3.get())
+      A2=int(txt4.get())
+      time=2*np.pi
+
+      amplitude =np.array([time,f1,f2,A1,A2])
+      np.savetxt('mycsv.csv',amplitude)
+      def readData():
+          q=np.loadtxt('mycsv.csv')
+          return q
+
+      a=readData() 
+
+
+
+      timep=np.arange(0,a[0],0.00001)
+      amp1=np.sin(timep*a[1])
+      
+      plot.subplot(4,2,1)#plot one Sine wave
+      plot.plot(timep, a[3]*amp1,color='darkorange')
+      plot.title('Sine wave')
+      plot.xlabel('Time')
+      plot.ylabel('Amplitude')
+      plot.grid(True, which='both',color='darkblue')
+      plot.axhline(y=0,color='darkblue' )
+
+
+
+      amp2=np.cos(timep*a[2])
+
+      plot.subplot(4,2,2)#plot second Cosine Wave
+      plot.plot(timep,a[4]*amp2,color='crimson')
+      plot.title('Cosine wave')
+      plot.xlabel('Time')
+      plot.ylabel('Amplitude')
+      plot.grid(True, which='both',color='darkblue')
+      plot.axhline(y=0, color='darkblue')
 
       plot.savefig('graph.png')
 
 button=Button(root,text="Plot",command=show)
 button.grid(column=0,row=5,pady=(10,0))
 
-button1=Button(root,text="save",command=save)
+button1=Button(root,text="Save",command=save)
 button1.grid(column=1,row=5,pady=(12,0),padx=(20,0))
 root.mainloop()
